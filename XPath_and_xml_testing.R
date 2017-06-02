50f6425f8d954dcbaa3e42879c03c6b9cbf65566
Eric Sangster <Eric.J.Sangster@kp.org>
2017-06-02 16:43:51 -0700

    Exploration of xml and quiz exercises

---
+++ XPath_and_xml_testing.R
@@ -0,0 +1,5 @@
+# test case from lecture
+simplUrl <- "https://www.w3schools.com/xml/simple.xml"
+download.file(simplUrl, destfile = bk <- tempfile(fileext = ".xml"))
+mnu <- xmlParse(bk)
+fd_nm <- xpathSApply(mnu, "//food/name", xmlValue)  #Will //name capture what I need?
