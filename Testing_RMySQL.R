9879175bcb85a44cea4c1c2cee1c18d68e025b2e
DSTKES <esangste@hotmail.com>
2017-06-06 21:26:40 -0700

    Successful connectionto hg19

--- Testing_RMySQL.R
+++ Testing_RMySQL.R
@@ -6,3 +6,13 @@ ucscDb <- dbConnect(MySQL(), user="genome",
                     host="genome-mysql.soe.ucsc.edu")  
 
 Result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb)
+
+Result[1:10,]
+
+
+# Connect to hg19 and listing tables
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19",
+                  host="genome-mysql.soe.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
